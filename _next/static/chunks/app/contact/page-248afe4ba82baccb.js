f8bb5cf76b898edf8c3c02f2c943f01d3c3a98b9
deploy <deploy@local>
2026-09-12 11:13:02 +0200

    Preview build 2026-09-12

---
+++ _next/static/chunks/app/contact/page-248afe4ba82baccb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{3652:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,579)),Promise.resolve().then(s.bind(s,6906)),Promise.resolve().then(s.bind(s,629))}},function(e){e.O(0,[972,57,971,117,744],function(){return e(e.s=3652)}),_N_E=e.O()}]);
